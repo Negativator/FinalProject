b68e07ab0bc71061c5119b6cdf7e24edfb0ce6b1
Negativator <aleksandar_mantov1991@abv.bg>
2020-12-13 20:17:31 +0200

    Home page and CSS

--- Quiz/views.py
+++ Quiz/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    a = HttpResponse(b'welcome to this section')
-    return a
+    return render(request, 'landing.html')
